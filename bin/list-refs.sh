694beb874bb37bc05f5f5aec6c5a64101c143648
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-05-13 14:46:36 -0500

    add DOI column to summary table; related to https://github.com/bat-literature/bat-literature.github.io/issues/2

--- bin/list-refs.sh
+++ bin/list-refs.sh
@@ -4,12 +4,12 @@
 #
 #
 
-cat <(echo "id,authors,date,title,journal")\
+cat <(echo "id,authors,date,title,journal,doi")\
  <(preston head\
  | preston cat\
  | grep "items[?]"\
  | grep hasVersion\
  | preston cat\
  | jq -c .[]\
- | jq --raw-output -c 'select(.data.creators != null) | [.links.alternate.href, (.data.creators | map(.lastName) | join(" | ")), ( .data.date, .data.title, .data.publicationTitle)] | @csv')
+ | jq --raw-output -c 'select(.data.creators != null) | [.links.alternate.href, (.data.creators | map(.lastName) | join(" | ")), ( .data.date, .data.title, .data.publicationTitle, .data.DOI)] | @csv')
 
