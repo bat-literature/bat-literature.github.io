b9d72662dcf45312f54f9c927a3319c1d1d99159
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-12-11 17:45:22 -0600

    add md5 signatures for pdf and corpus version

--- bin/list-refs.sh
+++ bin/list-refs.sh
@@ -9,12 +9,26 @@ set -x
 SCRIPT_DIR=$(dirname $0)
 HEAD=$(cat "${SCRIPT_DIR}/../HEAD")
 
-cat <(echo "id,authors,date,title,journal,doi")\
+mlr --csv join -j attachment\
+ -f\
+ <(cat <(echo "attachment,id,authors,date,title,journal,doi")\
  <(preston cat ${HEAD}\
  | grep "items[?]"\
  | grep hasVersion\
  | preston cat --algo md5\
  | jq -c .[]\
- | jq --raw-output -c 'select(.data.creators != null) | [.links.alternate.href, (.data.creators | map(.lastName) | join(" | ")), ( .data.date, .data.title, .data.publicationTitle, .data.DOI)] | @csv')\
- | tr '\t' ' '
-
+ | jq --raw-output -c 'select(.data.creators != null) | select(.links.attachment.href != null) | [.links.attachment.href, .links.alternate.href, (.data.creators | map(.lastName) | join(" | ")), ( .data.date, .data.title, .data.publicationTitle, .data.DOI)] | @csv'\
+ | sort)\
+ | tr '\t' ' ')\
+ <(cat <(echo "attachment,corpusId,attachmentId")\
+ <(preston cat ${HEAD}\
+ | grep "items[?]"\
+ | grep hasVersion\
+ | preston cat --algo md5\
+ | jq -c .[]\
+ | jq --raw-output --arg HEAD "$HEAD" 'select(.data.md5 != null) | [.links.self.href, $HEAD, .data.md5] | @csv'\
+ | sort))\
+ | mlr --csv reorder -e -f id,attachment,corpusId,attachmentId\
+ | sed -E 's+([a-f0-9]{32})$+hash://md5/\1+g'\
+ | mlr --csv reorder -e -f id,attachment,attachmentId,corpusId\
+ 
