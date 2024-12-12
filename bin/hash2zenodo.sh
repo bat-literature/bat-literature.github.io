2ae311288138029a0b25543771d1b87f99336558
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-12-12 13:53:55 -0600

    updated header doi -> alternativeDoi

--- bin/hash2zenodo.sh
+++ bin/hash2zenodo.sh
@@ -5,7 +5,7 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-cat <(echo "attachmentId,doi") <(preston track --data-dir "${SCRIPT_DIR}/../zenodo"\
+cat <(echo "attachmentId,alternativeDoi") <(preston track --data-dir "${SCRIPT_DIR}/../zenodo"\
  --algo md5\
  -f <(bash "${SCRIPT_DIR}/list-refs.sh" | mlr --csv cut -f attachmentId | tail -n+2 | xargs -I{} echo "https://zenodo.org/api/communities/batlit/records?q=%22{}%22&l=list&limit=1")\
  | grep hasVersion\
