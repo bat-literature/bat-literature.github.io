4c7ece17daa8b935a95796c8341c858086388046
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-10-07 10:36:46 -0500

    use lsid instead of pdf md5 to track zotero records in Zenodo

--- bin/track-zenodo-associations.sh
+++ bin/track-zenodo-associations.sh
@@ -9,8 +9,10 @@ DATA_DIR="${SCRIPT_DIR}/../zenodo"
 preston track --data-dir "${DATA_DIR}"\
  --algo md5\
  -f <(bash "${SCRIPT_DIR}/list-refs.sh"\
- | mlr --csv cut -f attachmentId\
+ | mlr --csv cut -f id\
  | tail -n+2\
+ | grep -oE '[A-Z0-9]+$'\
+ | sed 's+^+urn:lsid:zotero.org:groups:5435545:items:+g'\
  | xargs -I{} echo "https://zenodo.org/api/communities/batlit/records?q=%22{}%22&l=list&limit=1")
 
 preston head --data-dir "${DATA_DIR}" --algo md5\
