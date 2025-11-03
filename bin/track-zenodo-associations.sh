62c854e8f84f5333d04ec589ced732da03ed8846
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-11-03 13:24:42 -0600

    include Zotero group id in finding deposit associates

--- bin/track-zenodo-associations.sh
+++ bin/track-zenodo-associations.sh
@@ -18,14 +18,15 @@ DATA_DIR="${SCRIPT_DIR}/../zenodo"
 ZENODO_ENDPOINT_URL=${1:-https://zenodo.org/}
 ZENODO_COMMUNITY=${2:-batlit}
 
-preston track --data-dir "${DATA_DIR}"\
- --algo md5\
- -f <(bash "${SCRIPT_DIR}/list-refs.sh"\
- | mlr --csv cut -f id\
- | tail -n+2\
- | grep -oE '[A-Z0-9]+$'\
- | sed 's+^+urn:lsid:zotero.org:groups:5435545:items:+g'\
+preston track --data-dir "${DATA_DIR}" \
+ --algo md5 \
+ -f <(bash "${SCRIPT_DIR}/list-refs.sh" \
+ | mlr --csv cut -f id \
+ | tail -n+2 \
+ | grep -oE '[0-9]+/items/[A-Z0-9]+$' \
+ | tr '/' ':' \
+ | sed 's+^+urn:lsid:zotero.org:groups:+g' \
  | xargs -I{} echo "${ZENODO_ENDPOINT_URL}/api/communities/${ZENODO_COMMUNITY}/records?q=%22{}%22&l=list&limit=1")
 
-preston head --data-dir "${DATA_DIR}" --algo md5\
+preston head --data-dir "${DATA_DIR}" --algo md5 \
  > "${DATA_DIR}/HEAD"
